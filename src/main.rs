d56c44493fb5e9155cccb0d85d59a536e47375aa
Milo Weinberg <iapetus011@gmail.com>
2024-04-06 20:02:05 -0400

    Update main to allow running of any test function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use std::env;
+
 mod fibonacci;
 mod look_and_say;
 
 fn main() {
-    look_and_say::test();
-    // fibonacci::test();
+    let run = env::args().nth(1).expect("Specify the name of the file to run the test function of");
+
+    if run == "fibonacci" {
+        fibonacci::test();
+    } else if run == "look_and_say" {
+        look_and_say::test();
+    }
 }
